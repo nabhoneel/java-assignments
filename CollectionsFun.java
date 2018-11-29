3ba4c9464ed17fd83dab93c4603741370881fd8c
Nabhoneel Majumdar <nabhoneel.95@gmail.com>
2018-11-29 12:54:16 +0530

    shopping done; collections started

---
+++ CollectionsFun.java
@@ -0,0 +1,7 @@
+/*
+
+1. Create a calss Student having attributes Roll, Name, Course, Subject, Semester. Create a collection of Students.
+
+Display the students of course B.Tech, Subject=CS, Sem=3
+
+*/
